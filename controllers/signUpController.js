204614bdfb64a851deb47f057a68393a9b56e192
Lucas Viana <lucasvfv@gmail.com>
2022-05-11 17:34:49 -0300

    fix: encrypt password added

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,10 +1,14 @@
+import bcrypt from "bcrypt";
+
 import db from "../db.js";
+
 
 export async function signUp(req, res) {
     const { name, email, password } = req.body;
 
+    const securePassword = bcrypt.hashSync(password, 10);
     try {
-        await db.collection('users').insertOne({ name, email, password });
+        await db.collection('users').insertOne({ name, email, password: securePassword });
         res.status(201).send("Usuário cadastrado com sucesso");
     } catch (e) {
         res.send(500);
