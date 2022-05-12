44fe2cf59fa475a19ff7428826b825ec288de144
santannaizadora <santannaizadora@gmail.com>
2022-05-12 20:52:16 -0300

    feat: add token validation on get method of store

--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import getGames from "../controllers/storeController.js";
 import validateStore from "../middlewares/storeMiddleware.js";
+import { tokenValidate } from "../middlewares/gameMiddleware.js";
 
 const storeRouter = Router();
 
-storeRouter.get("/store", validateStore, getGames);
+storeRouter.get("/store", tokenValidate, validateStore, getGames);
 
 export default storeRouter;
